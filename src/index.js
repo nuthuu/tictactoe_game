379b00460ddf4cb471926ff8094ddc89e227f5e9
eric mwangi <nuthuerick147@gmail.com>
2024-05-06 15:36:15 +0300

    added event handlers and props

--- src/index.js
+++ src/index.js
@@ -5,16 +5,26 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square"
+        onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            Square: Array(9).fill(null),
+        };  
+    }
     renderSquare(i) {
-      return <Square />;
+      return <Square 
+      value={this.state.Square[i]}
+      onClick={() => this.handleClick(i)}
+      ></Square>;
     }
   
     render() {
